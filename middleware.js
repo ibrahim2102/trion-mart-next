53ae11d7d66bd0f8d179c988d4303ebd58c2d915
ibrahim2102 <mdibrahim91695@gmail.com>
2026-01-18 16:40:39 +0600

    still working

--- middleware.js
+++ middleware.js
@@ -1,17 +1,15 @@
 import { withAuth } from "next-auth/middleware";
-import { authOptions } from "./lib/auth";
 
-export default withAuth({
-  pages: {
-    signIn: "/login",
+export default withAuth(
+  function middleware(req) {
+    // You can add custom logic here later
   },
-  callbacks: {
-    authorized: ({ token, req }) => {
-      // Allow access if token exists
-      return !!token;
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
     },
-  },
-});
+  }
+);
 
 export const config = {
   matcher: ["/dashboard/:path*"],
